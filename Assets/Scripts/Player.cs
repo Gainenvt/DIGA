9b302a62049bcbb72a65494745905ecfdb66c77d
gainen <morishirogainen@gmail.com>
2026-07-27 19:33:59 +0200

    Initial commit

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,14 @@
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public GameObject player;
+    public float HP = 100f;
+    
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
